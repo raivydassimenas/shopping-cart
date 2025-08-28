c8c3a53dd8fa5344a38a5b512f1c3a19134843b9
Raivydas Šimėnas <raivydas.simenas@icloud.com>
2025-08-28 18:56:01 +0300

    Update the image location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
   return (
         <div className="container mx-auto">
           <h1 className="text-4xl font-bold mb-4 text-center">My Bookstore</h1>
-          <img src="../../assets/geilan-malet-bates-uRLECci4Hl4-unsplash.jpg" alt="Bookstore photo" />
+          <img src="/geilan-malet-bates-uRLECci4Hl4-unsplash.jpg" alt="Bookstore photo" />
           <p className="text-lg">This is a simple React application using TypeScript and Tailwind CSS that implements
             bookstore functionality</p>
         </div>
